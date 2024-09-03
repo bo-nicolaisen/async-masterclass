79992b02979078a392f620a532fdf8871e0d497b
bo-nicolaisen <bni@techcollege.dk>
2024-09-03 08:15:57 +0200

    start

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,77 @@
 // write cool JS hwere!!
+
+
+// normal function
+
+function gimmiStuff() {
+
+    return "stuff"
+}
+
+
+
+function getStuff() {
+
+    const result = gimmiStuff();
+    console.log(result);
+
+}
+
+getStuff()
+
+
+// promises og async functions
+
+function waitforSeconds() {
+
+    // promise oprettelse
+    return new Promise((resolve) => {
+
+        // timeout arrow function der executer når perioden er gået
+        setTimeout(() => {
+            console.log('timeout');
+            resolve('resolved');
+        }, 8000 // tid i ms);
+
+    });
+}
+
+async function asyncCall() {
+    console.log('calling waitforSeconds');
+
+    const result = await waitforSeconds();
+
+    console.log('asyncCall: ' + result);
+
+    // Expected output: "resolved"
+}
+
+
+//asyncCall();
+
+
+
+
+// fetch med promise
+
+
+
+// fetch med .then chain
+
+function getDog() {
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.table(data);
+
+        })
+        .catch((error) => {
+            console.log(error);
+
+        })
+}
+
+//getDog()
